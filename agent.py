c7a2111f020313fafbbaa4e1648e9ce820e16f07
J <jbboltz123@gmail.com>
2016-04-11 16:38:09 -0500

    Minor cosmetic changes
    Changed a few cosmetic issues.

--- agent.py
+++ agent.py
@@ -44,7 +44,7 @@ class LearningAgent(Agent):
 		if reward == 12:
 			self.win = self.win + 1
 			print self.win
-		
+
 		# TODO: Learn policy based on state, action, reward
 		print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 		
@@ -152,18 +152,19 @@ class LearningAgent(Agent):
 			q_values[each] = 0
 			
 		return q_values
-
+		
 def run():
-    """Run the agent for a finite number of trials."""
+	"""Run the agent for a finite number of trials."""
 
     # Set up environment and agent
-    e = Environment()  									# create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  					# create agent
-    e.set_primary_agent(a, enforce_deadline=True)  		# set agent to track
+	e = Environment()  									# create environment (also adds some dummy traffic)
+	a = e.create_agent(LearningAgent)  					# create agent
+	e.set_primary_agent(a, enforce_deadline=True)  		# set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.1)  				# reduce update_delay to speed up simulation
-    sim.run(n_trials=100)  								# press Esc or close pygame window to quit
+	sim = Simulator(e, update_delay=0.1)  				# reduce update_delay to speed up simulation
+	sim.run(n_trials=100)  								# press Esc or close pygame window to quit
+	
 
 
 if __name__ == '__main__':
